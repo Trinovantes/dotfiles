0d4b6c52890a7169f375b56f476bbc8a40a81be8
Stephen Li <stephenli.ca@gmail.com>
2013-11-27 12:24:54 -0500

    added gdbinit

---
+++ gdbinit
@@ -0,0 +1,5 @@
+set print pretty on
+set print array on
+set print demangle on
+set print asm-demangle on
+set print address on
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 dir="dotfiles"
-files="vim bash_profile bashrc inputrc vimrc git-completion.bash gitconfig git-prompt.sh"
+files="vim bash_profile bashrc gdbinit inputrc vimrc git-completion.bash gitconfig git-prompt.sh"
 
 for file in $files; do
     if [ -z "$1" ]; then
         echo Linking ~/.$file
-        ln -s $dir/$file ~/.$file
+        ln -sf $dir/$file ~/.$file
     else
         echo Unlinking ~/.$file
         unlink ~/.$file
