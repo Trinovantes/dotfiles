00aaa396d0d902b9aea8363462d19b3abf8c03aa
Stephen Li <stephenli.ca@gmail.com>
2013-12-18 15:08:50 -0500

    Update setup.sh
    got install/uninstall args backwards

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 dir="dotfiles"
 files="vim bash_profile bashrc gdbinit inputrc vimrc git-completion.bash gitconfig git-prompt.sh"
 
 for file in $files; do
     if [ "$1" == "unlink" ]; then
-        echo Linking ~/.$file
-        ln -sf $dir/$file ~/.$file
-    else
         echo Unlinking ~/.$file
         unlink ~/.$file
+    else
+        echo Linking ~/.$file
+        ln -sf $dir/$file ~/.$file
     fi
 done
 
