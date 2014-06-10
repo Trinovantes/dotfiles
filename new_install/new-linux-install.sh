eb3f5555a57fb0876be5a39e2920093b8bb87437
Stephen Li <stephenli.ca@gmail.com>
2014-06-09 21:24:54 -0700

    Added xzdec and updated sublime config file

--- new_install/new-linux-install.sh
+++ new_install/new-linux-install.sh
@@ -2,21 +2,10 @@
 
 apt-get update -y
 
-# Setup git
-apt-get install -y git
-apt-get install -y xclip 	# for copying ssh key to clipboard
-
-# Setup dotfiles
-cd ~/
-git clone https://github.com/Trinovantes/dotfiles.git
-cd ~/dotfiles
-sh install.sh
-
-# Setup vim
-apt-get install -y vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # General dev
+apt-get install -y git
+apt-get install -y xclip
+apt-get install -y xzdec
 apt-get install -y yum
 apt-get install -y curl
 apt-get install -y ack-grep
@@ -31,3 +20,21 @@ apt-get install -y valgrind
 add-apt-repository ppa:kilian/f.lux
 apt-get update
 apt-get install -y fluxgui
+
+# Setup dotfiles
+cd ~/
+git clone https://github.com/Trinovantes/dotfiles.git
+cd ~/dotfiles
+sh install.sh
+
+# Setup vim
+apt-get install -y vim
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Setup sublime
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+tar xf Sublime\ Text\ 2.0.2\ x64.tar.bz2
+mkdir -p ~/bin
+mv Sublime\ Text\ 2 ~/bin/
+sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+sudo cp sublime.desktop /usr/share/applications/
