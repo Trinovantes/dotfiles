f2c7d73b1d91a11670abc31b9c1a2abee2a8caad
Stephen Li <stephenli.ca@gmail.com>
2014-09-27 02:59:56 -0400

    Added script for installing sublime

---
+++ new_install/new-sublime-install.sh
@@ -0,0 +1,7 @@
+# Setup sublime
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+tar xf Sublime\ Text\ 2.0.2\ x64.tar.bz2
+mkdir -p ~/bin
+mv Sublime\ Text\ 2 ~/bin/
+sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
+sudo cp sublime.desktop /usr/share/applications/
