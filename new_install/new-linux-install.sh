8c1a9c14898be30ff582b7d07fd186fab8539459
Stephen Li <stephenli.ca@gmail.com>
2014-09-27 02:44:32 -0400

    Updated new install scripts
    - Removed getting files off internet from new-linux-install.sh because
      it's too much of a hassle to fix the files' owners (since the script
      was run in sudo)
    - Updated rvm install command
    - Updated Vundle version

--- new_install/new-linux-install.sh
+++ new_install/new-linux-install.sh
@@ -29,30 +29,7 @@ pip install httpie
 apt-get install -y npm
 npm install less
 
-# Web dev
-apt-get install -y php5-cli
-apt-get install -y mysql-server
-apt-get install -y phpmyadmin
-
 # Flux
 add-apt-repository ppa:kilian/f.lux
 apt-get update
 apt-get install -y fluxgui
-
-# Setup dotfiles
-cd ~/
-git clone https://github.com/Trinovantes/dotfiles.git
-cd ~/dotfiles
-sh install.sh
-
-# Setup vim
-apt-get install -y vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Setup sublime
-wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
-tar xf Sublime\ Text\ 2.0.2\ x64.tar.bz2
-mkdir -p ~/bin
-mv Sublime\ Text\ 2 ~/bin/
-sudo ln -s /opt/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
-sudo cp sublime.desktop /usr/share/applications/
--- new_install/new-ruby-install.sh
+++ new_install/new-ruby-install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y ruby
-curl -ssl https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | bash -s stable
 
 rvm get stable --ruby
 source /home/stephen/.rvm/scripts/rvm
