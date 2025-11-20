e7c798cea970f89955bb3e1a24f05eec155c26a0
Stephen Li <stephenli.ca@gmail.com>
2025-11-20 09:49:48 -0500

    Simplify new install script

--- new-install.bash
+++ new-install.bash
@@ -1,24 +1,4 @@
 #!/bin/bash
 
 apt-get update -y
-
-# General Dev
-apt-get install -y ack-grep
-apt-get install -y astyle
-apt-get install -y p7zip-full
-
-# C++ Dev
-apt-get install -y g++
-apt-get install -y clang
-apt-get install -y valgrind
-
-# Python Dev
-apt-get install -y python3-pip
-pip3 install httpie
-
-# Node Dev
-curl -sL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
-npm install -g nodemon
-npm install -g pnpm
-npm install -g n
+apt-get install -y build-essentials
